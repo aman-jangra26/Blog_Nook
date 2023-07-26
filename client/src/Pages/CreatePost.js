9e2b55170d5930e9e23520a4b8c89819c386c5fc
Aman Jangra <aman.12007267@lpu.in>
2023-07-26 22:16:19 +0530

    ook

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -1,28 +1,28 @@
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
-import {useState} from "react";
-import {Navigate} from "react-router-dom";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import Editor from "../Editor";
 
 export default function CreatePost() {
   const inputStyle = {
-    width: '100%',
-    padding: '8px',
-    margin: '10px 0',
-    borderRadius: '3px',
-    border: '1px solid #ccc',
+    width: "100%",
+    padding: "8px",
+    margin: "10px 0",
+    borderRadius: "3px",
+    border: "1px solid #ccc",
   };
 
   const buttonStyle = {
-    width: '100%',
-    padding: '10px',
-    margin: '10px 0',
-    borderRadius: '5px',
-    border: 'none',
-    backgroundColor: '#333333',
-    color: '#fff',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s',
+    width: "100%",
+    padding: "10px",
+    margin: "10px 0",
+    borderRadius: "5px",
+    border: "none",
+    backgroundColor: "#333333",
+    color: "#fff",
+    cursor: "pointer",
+    transition: "background-color 0.3s",
   };
 
   const [isButtonHovered, setButtonHovered] = useState(false);
@@ -34,53 +34,72 @@ export default function CreatePost() {
   const handleButtonLeave = () => {
     setButtonHovered(false);
   };
-  const [title,setTitle] = useState('');
-  const [summary,setSummary] = useState('');
-  const [content,setContent] = useState('');
-  const [files, setFiles] = useState('');
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
   const [redirect, setRedirect] = useState(false);
+
   async function createNewPost(ev) {
-    const data = new FormData();
-    data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
-    data.set('file', files[0]);
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/api/post', {
-      method: 'POST',
+
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.append("file", files[0]);
+
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
       body: data,
-      credentials: 'include',
+      credentials: "include",
     });
+
     if (response.ok) {
       setRedirect(true);
     }
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />
+    return <Navigate to={"/"} />;
   }
+
   return (
     <form onSubmit={createNewPost}>
-      <input type="title"
-             placeholder={'Title'}
-             style={inputStyle}
-             value={title}
-             onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-             placeholder={'Summary'}
-             style={inputStyle}
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-      style={inputStyle}
-             onChange={ev => setFiles(ev.target.files)} />
-      <Editor  value={content} onChange={setContent} />
-      <button  style={{
-                ...buttonStyle,
-                backgroundColor: isButtonHovered ? '#007bff' : '#333333',
-              }}
-              onMouseEnter={handleButtonHover}
-              onMouseLeave={handleButtonLeave}>Create post</button>
+      <input
+        type="text"
+        placeholder={"Title"}
+        style={inputStyle}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="text"
+        placeholder={"Summary"}
+        style={inputStyle}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type="file"
+        style={inputStyle}
+        onChange={(ev) => setFiles(ev.target.files)}
+      />
+      <ReactQuill
+        value={content}
+        onChange={setContent}
+        style={{ height: "400px", marginBottom: "20px" }}
+      />
+      <button
+        style={{
+          ...buttonStyle,
+          backgroundColor: isButtonHovered ? "#007bff" : "#333333",
+        }}
+        onMouseEnter={handleButtonHover}
+        onMouseLeave={handleButtonLeave}
+      >
+        Create post
+      </button>
     </form>
   );
-} 
+}
